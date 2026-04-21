7f88bf724992dab40c0c92f2c79df2df648ca9a8
Gianperfumo <gianlucaperfumo@gmail.com>
2026-04-21 17:19:29 -0300

    Atiende review: a que hice

--- operaciones.c
+++ operaciones.c
@@ -1,7 +1,7 @@
 #include "operaciones.h"
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
-
+/* hice que las variable tengan const*/
 int sumar(const int a,const int b) {
     return a + b;
 }
